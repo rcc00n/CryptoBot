b4c99a80d1fd36facb5292547f9cb0769b23d71c
KennethJ1 <114956555+KennethJ1@users.noreply.github.com>
2024-03-16 01:58:57 -0600

    Update install_dependencies.py

--- install_dependencies.py
+++ install_dependencies.py
@@ -5,7 +5,7 @@ def install(package):
     subprocess.check_call([sys.executable, "-m", "pip", "install", package])
 
 # List of packages to install
-packages = ['bybit', 'pandas', 'requests']  # Add other necessary packages here
+packages = ['bybit', 'pandas', 'requests', "python-dotenv"]  # Add other necessary packages here
 
 for package in packages:
     install(package)
